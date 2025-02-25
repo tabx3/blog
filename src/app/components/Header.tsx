3a8b086366034eafef4713000c2e9758f306ba58
tabx3 <tabx3way@gmail.com>
2025-02-26 03:42:59 +0800

    end1111

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -13,7 +13,7 @@ export default function Header() {
   return (
     <header className="w-full flex items-center justify-between py-4">
       <Logo />
-      <h1 className="text-2xl font-bold">李卫hello</h1>
+
       <button onClick={ToggleTheme}>
         {theme === "light" ? (
           <MoonIcon className="stroke-[#3f3e44]" />
