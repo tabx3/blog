11213536c9122ed2cc0fd3b5cc2e2c6bb8db24d9
tabx3 <tabx3way@gmail.com>
2025-02-25 23:13:55 +0800

    end111

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -5,11 +5,11 @@ export default function Hero() {
     <Link href="/blog/hore" className=" my-8  md:0 ">
       <div className="h-[180px] relative ">
         <Image
-          src="/images/hero.png"
+          src="/images/GgG9eTzWYAAkWx5.jpeg"
           alt="hero"
           width={1000}
           height={1000}
-          className="w-full h-full object-cover rounded-md overflow-hidden "
+          className="w-full h-full object-cover rounded-md overflow-hidden opacity-70 "
         />
         <div className="absolute -bottom-8 left-4 bg-white/50 backdrop-blur-sm    dark:bg-[#181818]/60 p-4 rounded-lg shadow-lg md:max-w-[80%] hidden md:block ">
           <button className=" text-xs bg-[#242424] w-fit py-1 px-2 rounded-sm text-white  mb-1">
